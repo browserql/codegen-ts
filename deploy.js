6febb8d4b61e2c7cebac572a9236af43ac47db83
pierrot <pierre@drouillet.com>
2021-11-18 21:04:08 +0100

    v1.0.10

--- deploy.js
+++ deploy.js
@@ -1,37 +1,67 @@
-const { exec } = require("child_process")
-const { readFile, writeFile } = require("fs/promises")
-const { promisify } = require("util")
-const semverInc = require('semver/functions/inc')
+const { exec } = require('child_process');
+const { readFile, writeFile } = require('fs/promises');
+const { promisify } = require('util');
+const semverInc = require('semver/functions/inc');
+const semverParse = require('semver/functions/parse');
 
 async function run(commitMessage) {
-  const pkg = JSON.parse((await readFile('package.json')).toString())
+  const pkg = JSON.parse((await readFile('package.json')).toString());
 
-  const nextVersion = semverInc(pkg.version, 'patch')
+  const nextVersion = semverInc(pkg.version, 'patch');
 
-  const nextBranch = `v${nextVersion}`
+  const nextBranch = `v${nextVersion}`;
 
-  await writeFile('package.json', JSON.stringify({
-    ...pkg,
-    version: nextVersion
-  }, null, 2))
+  await writeFile(
+    'package.json',
+    JSON.stringify(
+      {
+        ...pkg,
+        version: nextVersion,
+      },
+      null,
+      2
+    )
+  );
 
-  await promisify(exec)(`git checkout -b ${nextBranch}`)
+  const { major, minor, patch } = semverParse(nextBranch);
 
-  try {
-    await promisify(exec)('yarn tsc')
-  } catch (error) {
-    
-  }
+  const majorBranch = `v${major}`;
 
-  await promisify(exec)('git add .')
+  const minorBranch = `v${major}.${minor}`;
 
-  await promisify(exec)(`git commit -am "${nextBranch}"`)
+  await promisify(exec)(`git checkout -b ${nextBranch}`);
 
-  await promisify(exec)(`git push --set-upstream origin ${nextBranch}`)
+  await promisify(exec)('yarn build');
+
+  await promisify(exec)('git add .');
+
+  await promisify(exec)(`git commit --allow-empty -am "${nextBranch}"`);
+
+  await promisify(exec)(`git push --set-upstream origin ${nextBranch}`);
+
+  await promisify(exec)(`git checkout ${minorBranch}`);
+
+  await promisify(exec)(`git merge ${nextBranch}`);
+
+  await promisify(exec)('git add .');
+
+  await promisify(exec)(`git commit --allow-empty -am "${minorBranch}"`);
+
+  await promisify(exec)(`git push origin ${minorBranch}`);
+
+  await promisify(exec)(`git checkout ${majorBranch}`);
+
+  await promisify(exec)(`git merge ${minorBranch}`);
+
+  await promisify(exec)('git add .');
+
+  await promisify(exec)(`git commit --allow-empty -am "${majorBranch}"`);
+
+  await promisify(exec)(`git push origin ${majorBranch}`);
 
   console.log(nextBranch);
 }
 
-const [,, commit] = process.argv
+const [, , commit] = process.argv;
 
-run(commit)
+run(commit);
